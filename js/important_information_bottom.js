f302d4c861382649c6ea81772d687e8723e3fc46
Tonytosta <Tootoast@gmail.com>
2019-07-19 15:42:01 -0400

    Support bottom ISI

--- js/important_information_bottom.js
+++ js/important_information_bottom.js
@@ -14,30 +14,44 @@
 
             var container = drupalSettings.important_information.importantInformationBottom.container;
             var markup = drupalSettings.important_information.importantInformationBottom.markup;
-            
+            var verticalOffset = drupalSettings.important_information.importantInformationBottom.verticalOffset;
+
             if (!jQuery('#important-bottom-block-wrap').length) { //TODO: find a better way to check if its already appended
                 jQuery(container).append(markup);
             }
 
             window.onscroll = function(ev) {
+
+                var height = $('#important-bottom-block-wrap').height();
+
                 if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
                     // You're at the bottom of the page.
-                    //alert("You're at the bottom of the page! " + container);
 
-                    // Hide all open II blocks if not hidden - hide
-                    //$('block-importantinformationsidebar:visible').hide();
-                    //$('block-importantinformationfooter:visible').hide();
+                    // Show / hide all the stuff.
+                    $('#block-importantinformationsidebar:visible').hide();
+                    $('#block-importantinformationfooter:visible').hide();
+                    $('#important-bottom-block-wrap:hidden').show();
+                    // TODO: Make the selectors configurable
+                }
+
+                if ($('#important-bottom-block-wrap').is(":visible")) {
+
+                    var pos = window.innerHeight + window.scrollY + height + verticalOffset;
+                    var limit = document.body.offsetHeight ;
 
 
-                    // Append the II to the bottom of the page
-                    // TODO: Make the page DOM configurable
+                    if (pos < limit) {
+                        // Show / hide all the stuff.
+                        $('#block-importantinformationsidebar').show();
+                        $('#block-importantinformationfooter').show();
+                        $('#important-bottom-block-wrap').hide();
+
+                    }
 
                 }
+
             };
 
-
-            // Later in the script - Show it but only If it's not visible.
-            //$('.trigger:hidden').show();
         }
     };
 
